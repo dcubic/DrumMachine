5b5142db66058dce1243f53db29b1bcd965bebbe
Dominik Cubic <dcubic@students.cs.ubc.ca>
2021-05-26 02:59:11 -0700

    Using web URLs now instead of local audio clips

--- src/AudioPlayer.js
+++ src/AudioPlayer.js
@@ -2,15 +2,12 @@ export function playAudioClip(keyPressed) {
   let audioElementID = "audio-";
   audioElementID = audioElementID.concat(keyPressed);
   const audioElementCurrent = document.getElementById(audioElementID);
-  console.log(audioElementCurrent);
   const audioPromise = audioElementCurrent.play();
-  console.log("audioPromise");
-  console.log(audioPromise);
   if(audioPromise !== undefined) {
     audioPromise.then(() => {
-      console.log("Does the audio play?");
+      // console.log("Does the audio play?");
     }).catch(error => {
-      console.log("error");
+      // console.log("error");
     });
   }
 }
